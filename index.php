72b657e743fc634d72af5fa952528ccb7941cced
Mihai Ignat <45110804+emonelu@users.noreply.github.com>
2021-10-03 22:56:31 +0300

    Finished the slider selector, converted to php

---
+++ index.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Mihai Ignat Oficial</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Mihai Ignat Oficial Store">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- font Awesome Cdn -->
+	<script src="https://kit.fontawesome.com/e48d166edc.js" crossorigin="anonymous"></script>
+
+	<!-- Bootstrap cdn -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+	<!-- Owl Carousel File -->
+	<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+
+	<!-- CSS File -->
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+
+</head>
+
+<body>
+
+	<div class="super_container">
+		<!-- Header -->
+
+		<header class="header trans_300">
+			<!-- main Navigation -->
+
+			<div class="main_nav_container">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12 text-right">
+							<div class="logo_container">
+								<a href="#">mihai ignat <span>Oficial</span></a>
+							</div>
+							<nav class="navbar">
+								<ul class="navbar_menu">
+									<li><a href="#">Home</a></li>
+									<li><a href="#">Shop</a></li>
+									<li><a href="#">Promotion</a></li>
+									<li><a href="#">Pages</a></li>
+									<li><a href="#">About</a></li>
+									<li><a href="#">Contacts</a></li>
+								</ul>
+								<ul class="navbar_user">
+									<li><a href="#"><i class="fa fa-search" arial-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-user" arial-hidden="true"></i></a></li>
+									<li class="checkout">
+										<a href="#">
+											<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+											<span id="checkout_items" class="checkout_items">2</span>
+										</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+
+
+
+
+
+
+		<!-- Slider -->
+
+
+		<div class="main_slider" style="background-image: url(images/slider_1.jpg);">
+			<div class="container fill_hight">
+				<div class="row aligh-items-center fill_hight">
+					<div class="col">
+						<div class="main_slider_content">
+							<h6>Spring / Summer Colection 2021</h6>
+							<h1>Get up to 30% Off New Arrivers!</h1>
+							<div class="red_button shop_now_button"><a href="#">Shop Now</a></div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<!-- Banner -->
+
+
+		<div class="banner">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<div class="banner_item align-item-center" style="background-image: url(images/banner_men.jpg);">
+							<div class="banner_category">
+								<a href="#">Men</a>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-3">
+						<div class="banner_item align-item-center" style="background-image: url(images/banner_women.jpg);">
+							<div class="banner_category">
+								<a href="#">Women</a>
+							</div>
+						</div>
+					</div>
+
+
+					<div class="col-md-3">
+						<div class="banner_item align-item-center" style="background-image: url(images/banner_children.jpeg);">
+							<div class="banner_category">
+								<a href="#">Children</a>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-3">
+						<div class="banner_item align-item-center" style="background-image: url(images/banner_pets.jpeg);">
+							<div class="banner_category">
+								<a href="#">Pets</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<!-- New Arrivals -->
+
+		<div class="new_arrivals">
+			<div class="container">
+
+
+				<div class="row">
+					<div class="col text-center">
+						<div class="selection_title new_arrivals_title">
+							<h2>New Arrivals</h2>
+						</div>
+					</div>
+				</div>
+				<div class="row align-items-center">
+					<div class="col text_center">
+						<div class="new_arrivals_sorting">
+							<ul class="arrivals_grid_sorting clearfix button-group filters-button-group">
+								<li class="grid_sorting_button button d-flex flex-colum justify-content align-items-center active is-checked" data-filter="*">all</li>
+								<li class="grid_sorting_button button d-flex flex-colum justify-content align-items-center active is-checked" data-filter="*">all</li>
+								<li class="grid_sorting_button button d-flex flex-colum justify-content align-items-center active is-checked" data-filter="*">all</li>
+								<li class="grid_sorting_button button d-flex flex-colum justify-content align-items-center active is-checked" data-filter="*">all</li>
+
+							</ul>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+
+		<!-- Deal of the week -->
+
+
+
+		<!-- Benefit -->
+
+
+		<!-- Blogs -->
+
+
+		<!-- Newsletter -->
+
+
+		<!-- Footer -->
+	</div>
+
+	<!-- Bootstrap JS CDN -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+	<!-- jquery JS File -->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<!-- Isotope JS File -->
+	<script src="plugins/Isotope/isotope.pkgd.min.js"></script>
+	<!-- Owl Carousel JS File -->
+	<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+	<!-- Javascript File -->
+	<script src="js/custom.js"></script>
+</body>
+
+</html>
